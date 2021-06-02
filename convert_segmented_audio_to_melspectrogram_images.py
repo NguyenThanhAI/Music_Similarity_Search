34825ac0b96aee7a2ae6dad45adaf5e8628c53e2
Nguyen Thanh <Thanh.CEAA.K58.HUST@gmail.com>
2021-06-02 11:04:43 +0700

    Add catch exception

--- convert_segmented_audio_to_melspectrogram_images.py
+++ convert_segmented_audio_to_melspectrogram_images.py
@@ -76,41 +76,45 @@ if __name__ == '__main__':
     audio_files = audio_files[index:]
 
     for file in tqdm(audio_files):
-        #print("Audio file: {}".format(file))
-        y, sr = read_audio_file(file, args.sr_desired)
-        #print("y shape: {}".format(y.shape))
-        num_segments = math.ceil(y.shape[0] / num_ts_per_segments)
-        #print(num_segments)
-        #segments = np.split(y, num_segments)
-        for i in range(num_segments):
-            segment = y[i * num_ts_per_segments: (i + 1) * num_ts_per_segments]
-            #print("segment shape: {}".format(segment.shape))
+        try:
+            #print("Audio file: {}".format(file))
+            y, sr = read_audio_file(file, args.sr_desired)
+            #print("y shape: {}".format(y.shape))
+            num_segments = math.ceil(y.shape[0] / num_ts_per_segments)
+            #print(num_segments)
+            #segments = np.split(y, num_segments)
+            for i in range(num_segments):
+                segment = y[i * num_ts_per_segments: (i + 1) * num_ts_per_segments]
+                #print("segment shape: {}".format(segment.shape))
 
-            if segment.shape[0] < 250000:
-                continue
-            if segment.shape[0] >= num_ts_per_segments:
-                segment = segment[:num_ts_per_segments]
-            else:
-                segment = np.pad(segment, (0, num_ts_per_segments - segment.shape[0]), mode="constant")
+                if segment.shape[0] < 250000:
+                    continue
+                if segment.shape[0] >= num_ts_per_segments:
+                    segment = segment[:num_ts_per_segments]
+                else:
+                    segment = np.pad(segment, (0, num_ts_per_segments - segment.shape[0]), mode="constant")
 
-            mel_spectrogram = librosa.feature.melspectrogram(segment, sr=sr,
-                                                             n_fft=args.sample_frames,
-                                                             hop_length=args.hop_length,
-                                                             n_mels=args.n_mels)
+                mel_spectrogram = librosa.feature.melspectrogram(segment, sr=sr,
+                                                                 n_fft=args.sample_frames,
+                                                                 hop_length=args.hop_length,
+                                                                 n_mels=args.n_mels)
 
-            mel_spectrogram = np.log(mel_spectrogram + 1e-9)
-            img = scale_min_max(mel_spectrogram, 0, 255).astype(np.uint8)
-            img = np.flip(img, axis=0)
-            img = 255 - img
+                mel_spectrogram = np.log(mel_spectrogram + 1e-9)
+                img = scale_min_max(mel_spectrogram, 0, 255).astype(np.uint8)
+                img = np.flip(img, axis=0)
+                img = 255 - img
 
-            #print(img.shape)
+                #print(img.shape)
 
-            relpath = os.path.relpath(file, args.audio_dir)
-            save_dir = os.path.join(args.save_dir, os.path.dirname(relpath))
-            if not os.path.exists(save_dir):
-                os.makedirs(save_dir, exist_ok=True)
+                relpath = os.path.relpath(file, args.audio_dir)
+                save_dir = os.path.join(args.save_dir, os.path.dirname(relpath))
+                if not os.path.exists(save_dir):
+                    os.makedirs(save_dir, exist_ok=True)
 
-            cv2.imwrite(os.path.join(save_dir, os.path.splitext(os.path.basename(file))[0] + "_" + str(i) + ".jpg"), img)
+                cv2.imwrite(os.path.join(save_dir, os.path.splitext(os.path.basename(file))[0] + "_" + str(i) + ".jpg"), img)
+        except Exception as e:
+            print("Error: {}, filename: {}".format(e, file))
+            continue
 
         index += 1
         with open("index.txt", "w+") as f:
